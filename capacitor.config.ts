defdbf6668e2f4d05c05524df2ea3edd653d117e
FemilPatel <femilsabhaya678@gmail.com>
2024-10-16 19:52:06 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sudokugame',
+  webDir: 'dist'
+};
+
+export default config;
